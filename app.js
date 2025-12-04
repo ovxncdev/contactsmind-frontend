8401878f8180ad7008180950fe1c5923a644bfaa
ovxncdev <adeniyireal1icloud.com>
2025-12-04 13:47:41 -0800

    Fix onclick quote escaping

--- app.js
+++ app.js
@@ -749,13 +749,13 @@ function renderContacts() {
       <div class="contact-card-header">
         <h3 class="contact-name">${escapeHtml(contact.name)}</h3>
         <div class="contact-actions">
-          <button class="icon-btn-sm" onclick="editContact('${contact.id}')" title="Edit">
+          <button class="icon-btn-sm" onclick="editContact(&quot;${contact.id}&quot;)" title="Edit">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
               <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
             </svg>
           </button>
-          <button class="icon-btn-sm delete" onclick="deleteContact('${contact.id}')" title="Delete">
+          <button class="icon-btn-sm delete" onclick="deleteContact(&quot;${contact.id}&quot;)" title="Delete">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="3 6 5 6 21 6"></polyline>
               <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
